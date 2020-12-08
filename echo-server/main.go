bbd57c12d5fa223a25d45a5213fe06a12efc8b11
Víctor Peinado <vitojph@gmail.com>
2020-12-08 22:57:52 +0100

    Implement echo server with Copy

--- echo-server/main.go
+++ echo-server/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bufio"
+	"io"
 	"log"
 	"net"
 )
@@ -9,19 +9,9 @@ import (
 func echo(conn net.Conn) {
 	defer conn.Close()
 
-	reader := bufio.NewReader(conn)
-	s, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatalln("Unabel to read data")
+	if _, err := io.Copy(conn, conn); err != nil {
+		log.Fatalln("Unable to reasd/write data")
 	}
-	log.Printf("Received %d bytes: %s", len(s), s)
-
-	log.Println("Writing data")
-	writer := bufio.NewWriter(conn)
-	if _, err := writer.WriteString(s); err != nil {
-		log.Fatalln("Unable to write data")
-	}
-	writer.Flush()
 }
 
 func main() {
@@ -36,7 +26,8 @@ func main() {
 		conn, err := listener.Accept()
 		log.Println("Received connection")
 		if err != nil {
-			log.Fatalln("Unabel to accept connection")
+			log.Fatalln("Unable to accept connection")
+
 		}
 		go echo(conn)
 	}
